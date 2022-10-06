52815a44d73531b53a1d8d90368dec293f2f3c13
robs-am <roberta.amaro89@gmail.com>
2022-10-06 02:45:10 -0300

    add estilizacao do card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,8 +3,8 @@ import './styles.css'
 export function Card() {
     return (
         <div className='card'>
-            <strong>Roberta Amaro</strong>
-            <small>09:35</small>
+            <strong className='card-name'>Roberta Amaro</strong>
+            <small className='card-hour'>09:35</small>
         </div>
     )
 }
